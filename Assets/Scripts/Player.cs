caebbca461143d34fdc286162e4c12fefe80ae14
Kraktu <Duponthphilip15@gmail.com>
2019-11-04 14:45:37 +0100

    Deplacement basique

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[Tooltip("Number of meter by second")]
+	public float _speed;
+	Vector2 _velocity;
+	private void Update()
+	{
+		int horizontal = 0;
+		int vertical = 0;
+		if (Input.GetKey(KeyCode.Q))
+		{
+			horizontal -= 1;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			horizontal += 1;
+		}
+		if (Input.GetKey(KeyCode.Z))
+		{
+			vertical += 1;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			vertical -= 1;
+		}
+		_velocity= new Vector2(horizontal,vertical);
+		transform.Translate(_velocity * Time.deltaTime * _speed);
+	}
+}
