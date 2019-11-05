db517feedc4dc34946aa905cfe377f17fb40198f
Kraktu <Duponthphilip15@gmail.com>
2019-11-05 12:08:16 +0100

    Double Jump, WallJump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 	[Tooltip("Number of meter by second")]
 	public float _speed;
 	public float _gravity;
+	public float _jumpForce;
+	public int _maxJump;
+	int _jumpCount=0;
 
 	Vector2 _velocity;
 	MovementController _movementController;
@@ -16,7 +19,16 @@ public class Player : MonoBehaviour
 	}
 	private void Update()
 	{
+		
 		int horizontal = 0;
+		if (_movementController._collision.bottom || _movementController._collision.top)
+		{
+			_velocity.y = 0;
+			if(_movementController._collision.bottom)
+				_jumpCount = 0;
+		}
+
+
 		if (Input.GetKey(KeyCode.Q))
 		{
 			horizontal -= 1;
@@ -25,13 +37,34 @@ public class Player : MonoBehaviour
 		{
 			horizontal += 1;
 		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (_movementController._collision.left || _movementController._collision.right&&(Input.GetKey(KeyCode.Q)||Input.GetKey(KeyCode.D)))
+			{
+				_jumpCount = _maxJump - 1;
+				Jump();
+				if (_movementController._collision.left)
+					horizontal = 30;
+				if (_movementController._collision.right)
+					horizontal = -30;
+			}
+			else if (_jumpCount < _maxJump)
+			{
+				Jump();
+			}
+
+		}
 
 		_velocity.x = horizontal * _speed;
 
-		if (_movementController._collision.bottom)
-			_velocity.y = 0;
+
 		_velocity.y += _gravity * Time.deltaTime * -1f;
 
 		_movementController.Move(_velocity*Time.deltaTime);
+	}
+	public void Jump()
+	{
+		_velocity.y = _jumpForce;
+		_jumpCount++;
 	}
 }
