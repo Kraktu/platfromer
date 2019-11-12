f3469c8fc67b763e36febd7bf15e8152a5877a16
Kraktu <Duponthphilip15@gmail.com>
2019-11-12 10:15:16 +0100

    Singleton

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Difficulty { easy,normal,hard}
 public class Game : MonoBehaviour
 {
+	public Difficulty _difficulty;
+	static public Game Instance { get; private set; }
 	// Start is called before the first frame update
 	void Awake()
 	{
+		if (Instance!=null&& Instance!=this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
 		Debug.Log("Start game");
 		QualitySettings.vSyncCount = 0;  // VSync must be disabled to allow targetFrameRate
 		Application.targetFrameRate = 60;
 	}
-
-
 }
