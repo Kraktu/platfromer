248c9fa728140d017f52bb4883a79df7560bc09e
Kraktu <Duponthphilip15@gmail.com>
2019-11-15 15:32:27 +0100

    Resolution and fullscreen and easteregg

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -6,11 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    void Update()
+	int _rotation=50;
+	public string[] _sentence;
+	int _choosedSentence;
+
+	private void Start()
+	{
+		_choosedSentence = 0;
+	}
+	void Update()
     {
+		transform.Rotate(0, 0, _rotation * Time.deltaTime);
+		
 		if (Input.GetKeyDown(KeyCode.U))
 		{
 			SceneManager.LoadScene("MainMenu");
+		}
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			_rotation = 100;
+		}
+		if(Input.GetKeyUp(KeyCode.Mouse0))
+		{
+			_rotation = 50;
+		}
+		if(Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			_choosedSentence=(int)Random.Range(0,_sentence.Length);
+
+			GetComponent<TextMesh>().text = _sentence[_choosedSentence];
 		}
     }
 }
--- Assets/Scripts/MenuControler.cs
+++ Assets/Scripts/MenuControler.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuControler : MonoBehaviour
 {
+	public TextMesh _easterEgg;
 	public List<MenuItem> _menuItems;
 	List<MenuItem> _initialMenuItems;
 	public Color _activeColor, _inactiveColor;
 	MenuItem _activeMenuItem;
-	int _activeIndex;
-    void Start()
+	int _activeIndex,_activeResolution;
+	Vector2[] Resolution = new Vector2[3];
+	bool _isFullScreen = false;
+	void Start()
     {
+		Resolution[0] = new Vector2(1920, 1080);
+		Resolution[1] = new Vector2(1280, 720);
+		Resolution[2] = new Vector3(720, 405);
 		_initialMenuItems = _menuItems;
+		_activeResolution = 1;
 		InitMenu();   
     }
 
@@ -20,8 +27,7 @@ public class MenuControler : MonoBehaviour
     void Update()
     {
 		MenuNavigation();
-	
-    }
+	}
 	void InitMenu()
 	{
 		_activeIndex = 0;
@@ -32,7 +38,10 @@ public class MenuControler : MonoBehaviour
 			textMesh.text = menuItem._label;
 			menuItem.SetInactive(_inactiveColor);
 			menuItem.InitAllSubItems();
-			
+			if (menuItem.action== MenuAction.Resolution)
+			{
+				menuItem.GetComponentInChildren<TextMesh>().text = "Resolution : " + Resolution[_activeResolution].x + " X " + Resolution[_activeResolution].y;
+			}
 		}
 		_activeMenuItem.SetActive(_activeColor);
 	}
@@ -49,6 +58,15 @@ public class MenuControler : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
 			MenuSelected();
+		}
+		if (_activeMenuItem.action== MenuAction.Resolution&& (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q)))
+		{
+
+			ActionResolution(left: true);
+		}
+		if (_activeMenuItem.action == MenuAction.Resolution && (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)))
+		{
+			ActionResolution(left: false);
 		}
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
@@ -68,6 +86,28 @@ public class MenuControler : MonoBehaviour
 			}
 			InitMenu();
 		}
+		#region EasterEgg
+		if (Input.GetKey(KeyCode.P))
+		{
+			if (Input.GetKey(KeyCode.O))
+			{
+				if (Input.GetKey(KeyCode.U))
+				{
+					if (Input.GetKey(KeyCode.E))
+					{
+						if (Input.GetKey(KeyCode.T))
+						{
+							_easterEgg.gameObject.SetActive(true);
+						}
+					}
+				}
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.P) || Input.GetKeyUp(KeyCode.O) || Input.GetKeyUp(KeyCode.U) || Input.GetKeyUp(KeyCode.E) || Input.GetKeyUp(KeyCode.T))
+		{
+			_easterEgg.gameObject.SetActive(false);
+		}
+		#endregion
 	}
 	void MenuSelected()
 	{
@@ -88,18 +128,18 @@ public class MenuControler : MonoBehaviour
 			case MenuAction.Continue:
 				ActionContinue();
 				break;
-			case MenuAction.Resolution:
-				ActionResolution();
-				break;
 			case MenuAction.FullScreen:
 				ActionFullScreen();
+				break;
+			case MenuAction.Resolution:
+				ApplyResolution();
 				break;
 			default:
 				break;
 		}
-
-
 	}
+
+	#region Actions
 	void ActionGoDeeper()
 	{
 		
@@ -136,15 +176,37 @@ public class MenuControler : MonoBehaviour
 	void ActionContinue()
 	{
 
-
 	}
-	void ActionResolution()
+	void ActionResolution(bool left)
 	{
+		if (left==true)
+		{
+			_activeResolution--;
+			if (_activeResolution<0)
+			{
+				_activeResolution = Resolution.Length - 1;
+			}
+		}
+		if (left==false)
+		{
+			_activeResolution++;
+			if (_activeResolution > Resolution.Length-1)
+			{
+				_activeResolution = 0;
+			}
+		}
+		_activeMenuItem.GetComponentInChildren<TextMesh>().text = "Resolution : " + Resolution[_activeResolution].x + " X " + Resolution[_activeResolution].y;
+	}
+	#endregion
 
+	void ApplyResolution()
+	{
+		Screen.SetResolution((int)Resolution[_activeResolution].x, (int)Resolution[_activeResolution].y, _isFullScreen) ;
 	}
 	void ActionFullScreen()
 	{
-
+		_isFullScreen = !_isFullScreen;
+		Screen.SetResolution((int)Resolution[_activeResolution].x, (int)Resolution[_activeResolution].y, _isFullScreen);
 	}
 	private void NavigateUpDown(bool goDown)
 	{
